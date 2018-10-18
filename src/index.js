2a979829f55d4f765c9aeab102ab9dcd5938c5fa
Navdeep Singh <me@navdeepsingh.ca>
2018-10-18 16:54:49 -0700

    Update to use latest versions of dependencies Add linting and fix errors

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
-'use strict';
+import { JSDOM } from 'jsdom';
 
-import dom from 'jsdom';
+const SGPC_URL = 'http://old.sgpc.net/hukumnama/sgpconlinehukamnama.asp';
 
-const SGPC_URL = `http://old.sgpc.net/hukumnama/sgpconlinehukamnama.asp`;
+module.exports = () => new Promise((resolve, reject) => {
+  JSDOM.fromURL(SGPC_URL)
+    .then(dom => {
+      const { document } = dom.window;
 
-module.exports = (options = {  }) => new Promise((resolve, reject) => dom.env(SGPC_URL, (err, { document }) => {
-  try { 
-    const cleanTable = table => table.textContent.replace(/[\t\r\n]/g, '').trim();
-    const tables = [...document.querySelectorAll('table')].splice(1).map(cleanTable);
-    const [ gurbani, gurbaniAng, punjabi, english, englishAng ] = tables;
+      try {
+        const cleanTable = table => table.textContent.replace(/[\t\r\n]/g, '').trim();
+        const tables = [...document.querySelectorAll('table')].splice(1).map(cleanTable);
+        // eslint-disable-next-line no-unused-vars
+        const [gurbani, gurbaniAng, punjabi, english, englishAng] = tables;
 
-    resolve({
-      content: tables.join('\n'),
-      gurbani, punjabi, english,
-      ang: parseInt(/Page:[ ]*([\d ]+)/.exec(englishAng)[1]),
-      date: document.querySelector('font[face="Georgia, Times New Roman, Times, serif"]').textContent.trim().slice(1,-1),
-      url: SGPC_URL
+        resolve({
+          content: tables.join('\n'),
+          gurbani,
+          punjabi,
+          english,
+          ang: parseInt(/Page:[ ]*([\d ]+)/.exec(englishAng)[1], 10),
+          date: document.querySelector('font[face="Georgia, Times New Roman, Times, serif"]').textContent.trim().slice(1, -1),
+          url: SGPC_URL
+        });
+      } catch (e) {
+        reject(e);
+      }
     });
-
-  } catch (e) {
-    reject(e);
-  }
-}));
+});
--- test.js
+++ test.js
@@ -1,10 +1,11 @@
-'use strict';
-
-const hukam = require('./index.js');
+const hukam = require('./src/index.js');
 
 hukam()
-  .then(({ ang, content, english, gurbani, punjabi }) => {
-    if (ang < 1 || ang > 1430) { console.log("Invalid Ang ", ang); }
-    else { console.log("All tests passed!"); }
+  .then(({ ang }) => {
+    if (ang < 1 || ang > 1430) {
+      console.log('Invalid Ang ', ang);
+    } else {
+      console.log('All tests passed!');
+    }
   })
   .catch(err => console.log(err));
