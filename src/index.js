933a15b0ec4049f1ff771e625e740e78de443a44
Navdeep Singh <me@navdeepsingh.ca>
2018-10-30 08:11:52 -0700

    ES6 module export

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { JSDOM } from 'jsdom';
 
 const SGPC_URL = 'http://old.sgpc.net/hukumnama/sgpconlinehukamnama.asp';
 
-module.exports = () => new Promise((resolve, reject) => {
+const hukamnama = () => new Promise((resolve, reject) => {
   JSDOM.fromURL(SGPC_URL)
     .then(dom => {
       const { document } = dom.window;
@@ -10,7 +10,6 @@ module.exports = () => new Promise((resolve, reject) => {
       try {
         const cleanTable = table => table.textContent.replace(/[\t\r\n]/g, '').trim();
         const tables = [...document.querySelectorAll('table')].splice(1).map(cleanTable);
-        // eslint-disable-next-line no-unused-vars
         const [gurbani, gurbaniAng, punjabi, english, englishAng] = tables;
 
         resolve({
@@ -28,3 +27,5 @@ module.exports = () => new Promise((resolve, reject) => {
     })
     .catch(e => reject);
 });
+
+export default hukamnama;
